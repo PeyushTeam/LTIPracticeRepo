2e46ab8eebce1918ccac91a70f2cdc50072a236b
peyushgoelLTI <peyush.goel@lntinfotech.com>
2020-04-03 12:25:49 +0530

    adding

--- spring-mvc-example/src/main/java/com/journaldev/spring/controller/HomeController.java
+++ spring-mvc-example/src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -27,6 +27,9 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 
 		model.addAttribute("serverTime", formattedDate);
+		
+		model.addAttribute("serverTime", formattedDate);
+
 
 		return "home";
 	}
